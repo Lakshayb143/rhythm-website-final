c80d2c02d682a223c069c34558aa5976dd77928b
Lakshay Bhatia <coderlakshay289143@gmail.com>
2024-11-07 09:53:59 +0530

    Gallery updated

---
+++ src/screens/EmblaCarousel.jsx
@@ -0,0 +1,157 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import axios from "axios";
+
+
+
+const TWEEN_FACTOR_BASE = 0.2;
+
+const EmblaCarousel = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const tweenFactor = useRef(0);
+  const tweenNodes = useRef([]);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const setTweenNodes = useCallback((emblaApi) => {
+    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
+      return slideNode.querySelector(".embla__parallax__layer");
+    });
+  }, []);
+
+  const setTweenFactor = useCallback((emblaApi) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenParallax = useCallback((emblaApi, eventName) => {
+    const engine = emblaApi.internalEngine();
+    const scrollProgress = emblaApi.scrollProgress();
+    const slidesInView = emblaApi.slidesInView();
+    const isScrollEvent = eventName === "scroll";
+
+    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+      let diffToTarget = scrollSnap - scrollProgress;
+      const slidesInSnap = engine.slideRegistry[snapIndex];
+
+      slidesInSnap.forEach((slideIndex) => {
+        if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+        if (engine.options.loop) {
+          engine.slideLooper.loopPoints.forEach((loopItem) => {
+            const target = loopItem.target();
+
+            if (slideIndex === loopItem.index && target !== 0) {
+              const sign = Math.sign(target);
+
+              if (sign === -1) {
+                diffToTarget = scrollSnap - (1 + scrollProgress);
+              }
+              if (sign === 1) {
+                diffToTarget = scrollSnap + (1 - scrollProgress);
+              }
+            }
+          });
+        }
+
+        const translate = diffToTarget * (-1 * tweenFactor.current) * 100;
+        const tweenNode = tweenNodes.current[slideIndex];
+        tweenNode.style.transform = `translateX(${translate}%)`;
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setTweenNodes(emblaApi);
+    setTweenFactor(emblaApi);
+    tweenParallax(emblaApi);
+
+    emblaApi
+      .on("reInit", setTweenNodes)
+      .on("reInit", setTweenFactor)
+      .on("reInit", tweenParallax)
+      .on("scroll", tweenParallax)
+      .on("slideFocus", tweenParallax);
+  }, [emblaApi, tweenParallax]);
+
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const API = "http://13.233.201.180:8081/getImages";
+
+  useEffect(() => {
+    const FetchImages = async () => {
+      try {
+        const response = await axios.get(API);
+        setImages(response.data);
+        // setLoading(false);
+      } catch (error) {
+        // setLoading(false);
+        setError("Cannot fetch images due to error", error);
+      }
+    };
+    FetchImages();
+  }, []);
+
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {images.map((index) => (
+            <div className="embla__slide" >
+              <div className="embla__parallax">
+                <div className="embla__parallax__layer">
+                  <img
+                    className="embla__slide__img embla__parallax__img"
+                    src={index.image} 
+                    alt="Your alt text"
+                    
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
--- src/screens/gallery.jsx
+++ src/screens/gallery.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import Navbar from "./navbar.jsx";
+import axios from "axios";
+import "../style/gallery.css";
+import EmblaCarousel from "./EmblaCarousel.jsx";
 
 const Gallery = () => {
-    return (
-        <div className="h-screen w-screen bg-background-color overflow-x-hidden">
-            <Navbar/>
-        </div>
-    )
-}
+//   const [images, setImages] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+  const OPTIONS = { dragFree: true, loop: true }
+  const SLIDE_COUNT = 10;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
+//   const API = "http://13.233.201.180:8081/getImages";
+
+//   useEffect(() => {
+//     const FetchImages = async () => {
+//       try {
+//         const response = await axios.get(API);
+//         setImages(response.data);
+//         setLoading(false);
+//         console.log(images);
+//       } catch (error) {
+//         setLoading(false);
+//         setError("Cannot fetch images due to error", error);
+//       }
+//     };
+//     FetchImages();
+//   }, []);
+
+  return (
+    <div className="h-screen w-screen bg-background-color overflow-x-hidden">
+      <Navbar />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+    </div>
+  );
+};
 
 export default Gallery;
