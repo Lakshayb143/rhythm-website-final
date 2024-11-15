ccb694f3ddbe995ed5736e296fabe4c34d6df636
Alok Pandit <alokpandita3@gmail.com>
2024-11-15 20:02:41 +0530

    Added Rhythm 2k24 Sponsors (UPDATED HAWAI ADDA)

--- src/screens/SponsersBody.jsx
+++ src/screens/SponsersBody.jsx
@@ -1,5 +1,6 @@
 import '../style/sponsorsBody.css';
 import sponsor from '../assets/images/sponsor.jpeg';
+import hwai from '../assets/images/HwaiAdda.png';
 import location from '../assets/images/location.png';
 
 const SponsorsDiv = () => {
@@ -10,11 +11,27 @@ const SponsorsDiv = () => {
                 <h3>NAV GRAMIN, MURTHAL
                 </h3>
                 <div className="sponsor-location">
-                    <img src ={location} alt="location"/>
-                    <a href="https://maps.app.goo.gl/icwZdnuQ4P1XrKs96">
+                    <img src={location} alt="location"/>
+                    <a href="https://maps.app.goo.gl/icwZdnuQ4P1XrKs96" target='_blank' rel="noopener noreferrer">
                         Click here for more info
                     </a>
                 </div>
+
+
+            </div>
+
+            <div className="sponsor-body-card">
+                <img src={hwai} alt="rhythm_sponsor"/>
+                <h3>HAWAI ADDA, MURTHAL
+                </h3>
+                <div className="sponsor-location">
+                    <img src={location} alt="location"/>
+                    <a href="https://maps.app.goo.gl/C21SxQBy4MWtzX8s7" target='_blank' rel="noopener noreferrer">
+                        Click here for more info
+                    </a>
+                </div>
+
+
             </div>
         </div>
     )
