c099049ada6baf028429f8d9d40300647c7b72e8
Alok Pandit <alokpandita3@gmail.com>
2024-11-16 12:22:23 +0530

    Added Rhythm 2k24 Sponsors (UPDATED HAWAI ADDA)

--- src/screens/SponsersBody.jsx
+++ src/screens/SponsersBody.jsx
@@ -6,19 +6,6 @@ import location from '../assets/images/location.png';
 const SponsorsDiv = () => {
     return (
         <div className="sponsor-body-container">
-            <div className="sponsor-body-card">
-                <img src={sponsor} alt="rhythm_sponsor"/>
-                <h3>NAV GRAMIN, MURTHAL
-                </h3>
-                <div className="sponsor-location">
-                    <img src={location} alt="location"/>
-                    <a href="https://maps.app.goo.gl/icwZdnuQ4P1XrKs96" target='_blank' rel="noopener noreferrer">
-                        Click here for more info
-                    </a>
-                </div>
-
-
-            </div>
 
             <div className="sponsor-body-card">
                 <img src={hwai} alt="rhythm_sponsor"/>
@@ -27,6 +14,20 @@ const SponsorsDiv = () => {
                 <div className="sponsor-location">
                     <img src={location} alt="location"/>
                     <a href="https://maps.app.goo.gl/C21SxQBy4MWtzX8s7" target='_blank' rel="noopener noreferrer">
+                        Click here for more info
+                    </a>
+                </div>
+
+
+            </div>
+
+            <div className="sponsor-body-card">
+                <img src={sponsor} alt="rhythm_sponsor"/>
+                <h3>NAV GRAMIN, MURTHAL
+                </h3>
+                <div className="sponsor-location">
+                    <img src={location} alt="location"/>
+                    <a href="https://maps.app.goo.gl/icwZdnuQ4P1XrKs96" target='_blank' rel="noopener noreferrer">
                         Click here for more info
                     </a>
                 </div>
--- src/screens/gallery.jsx
+++ src/screens/gallery.jsx
@@ -5,30 +5,30 @@ import "../style/gallery.css";
 import EmblaCarousel from "./EmblaCarousel.jsx";
 
 const Gallery = () => {
-//   const [images, setImages] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
+   const [images, setImages] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
 
   const OPTIONS = { dragFree: true, loop: true }
   const SLIDE_COUNT = 10;
   const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
-//   const API = "http://13.233.201.180:8081/getImages";
+  const API = "http://13.233.201.180:8081/getImages";
 
-//   useEffect(() => {
-//     const FetchImages = async () => {
-//       try {
-//         const response = await axios.get(API);
-//         setImages(response.data);
-//         setLoading(false);
-//         console.log(images);
-//       } catch (error) {
-//         setLoading(false);
-//         setError("Cannot fetch images due to error", error);
-//       }
-//     };
-//     FetchImages();
-//   }, []);
+  useEffect(() => {
+     const FetchImages = async () => {
+       try {
+        const response = await axios.get(API);
+        setImages(response.data);
+        setLoading(false);
+        console.log(images);
+       } catch (error) {
+         setLoading(false);
+         setError("Cannot fetch images due to error", error);
+      }
+    };
+     FetchImages();
+  }, []);
 
   return (
     <div className="h-screen w-screen bg-background-color overflow-x-hidden">
